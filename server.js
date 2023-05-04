68d4625f1ae291ad363bfb7d78f4dc478d6811e8
Yash Raj Mishra <yashrm1406@gmail.com>
2023-05-04 18:28:28 +0530

    added subquestions requests

--- server.js
+++ server.js
@@ -31,15 +31,15 @@ app.get('/:category/:questionId/:optionId', (req, res) => {
   const questionId = req.params.questionId;
   const optionId = req.params.optionId;
   var questions = 'questions_AC';
+  var subQuestions = 'subquestions_AC';
 
   console.log(category + " " + optionId);
 
-  if (category === "ns") questions = 'questions_NS';
-  if (category === "gc") questions = 'questions_GC';
-  if (category === "lg") questions = 'questions_LG';
+  if (category === "ns") {questions = 'questions_NS'; subQuestions = 'subquestions_NS';};
+  if (category === "gc") {questions = 'questions_GC'; subQuestions = "NULL";};
+  if (category === "lg") {questions = 'questions_LG'; subQuestions = "NULL";};
 
   const query = `MATCH (n:${questions} {questionId:${questionId}})-[r:\`${optionId}\`]->(m:${questions}) RETURN m`;
-
   const session = driver.session({ database: 'neo4j' }); // Create a new session
   session.run(query, { questions: questions,questionId: questionId, optionId: optionId })
   .then((result) => {
@@ -65,33 +65,90 @@ app.get('/:category/:questionId/:optionId', (req, res) => {
       optionDCsc: result.records[0]._fields[0].properties.optionDCsc,
       optionECsc: result.records[0]._fields[0].properties.optionECsc,
     };
-    if (req.headers.accept.includes('application/json')) {
-      res.status(200).json({
-        Question: question,
-      });
-    } else {
-      res.render('index', {
-        Question: question,
-      });
-    }
+    session.close()
+    const session2 = driver.session({ database: 'neo4j' }); // Create a new session
+    const subQuery = `MATCH (n:${subQuestions}) where n.questionNo=$questionId return n`
+    session2.run(subQuery, { subQuestions: subQuestions,questionId:question.questionNumber })
+    .then((result)=>{
+      if(result.records.length === 0 ){
+        if (req.headers.accept.includes('application/json')) {
+          res.status(200).json({
+            Question: question,
+          }); 
+        }else {
+          res.render('index', {
+            Question: question,
+          });
+        }
+      } else {
+        var subQuestionsArr =[];
+        result.records.forEach((record) => {
+          var subQuestion = {
+            id: record._fields[0].identity.low,
+            type: record._fields[0].properties.type,
+            questionNumber:record._fields[0].properties.questionNo.low,
+            title: record._fields[0].properties.questionTitle,
+            optionA: record._fields[0].properties.optionA,
+            optionB: record._fields[0].properties.optionB,
+            optionC: record._fields[0].properties.optionC,
+            optionD: record._fields[0].properties.optionD,
+            optionE: record._fields[0].properties.optionE,
+            optionACsc: record._fields[0].properties.optionACsc,
+            optionBCsc: record._fields[0].properties.optionBCsc,
+            optionCCsc: record._fields[0].properties.optionCCsc,
+            optionDCsc: record._fields[0].properties.optionDCsc,
+            optionECsc: record._fields[0].properties.optionECsc
+          }
+          subQuestionsArr.push(subQuestion);
+        })
+          // console.log(subQuestion);
+          if (req.headers.accept.includes('application/json')) {
+            res.status(200).json({
+              Question: question,
+              subQuestions:subQuestionsArr
+            }); 
+          }else {
+            res.render('index', {
+              Question: question,
+              subQuestions:subQuestionsArr
+            });
+          }
+      }    
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      session2.close(); // Close the session
+    });
+    // if (req.headers.accept.includes('application/json')) {
+    //   res.status(200).json({
+    //     Question: question,
+    //   }); 
+    // } else {
+    //   res.render('index', {
+    //     Question: question,
+    //   });
+    // }
   })
   .catch((err) => {
     console.log(err);
   })
-  .finally(() => {
-    session.close(); // Close the session
-  });
+  // .finally(() => {
+  //   session.close(); // Close the session
+  // });
     
 });
 
 
 app.get('/:category', (req,res) => {
   const category = req.params.category;
-
   var questions = 'questions_AC';
-  if (category === "ns") questions = 'questions_NS';
-  if (category === "gc") questions = 'questions_GC';
-  if (category === "lg") questions = 'questions_LG';
+  var subQuestions = 'subquestions_AC';
+
+  if (category === "ns") {questions = 'questions_NS'; subQuestions = 'subquestions_NS';};
+  if (category === "gc") {questions = 'questions_GC'; subQuestions = "NULL";};
+  if (category === "lg") {questions = 'questions_LG'; subQuestions = "NULL";};
 
   const questionId = 1;
   const query = `MATCH (n:${questions} {questionId: $questionId}) RETURN n`;
@@ -122,15 +179,62 @@ app.get('/:category', (req,res) => {
       optionDCsc: result.records[0]._fields[0].properties.optionDCsc,
       optionECsc: result.records[0]._fields[0].properties.optionECsc,
     };
-    if (req.headers.accept.includes('application/json')) {
-      res.status(200).json({
-        Question: question,
-      });
-    } else {
-      res.render('index', {
-        Question: question,
-      });
-    }
+    session.close()
+    const session2 = driver.session({ database: 'neo4j' }); // Create a new session
+    const subQuery = `MATCH (n:${subQuestions}) where n.questionNo=$questionId return n`
+    session2.run(subQuery, { subQuestions: subQuestions,questionId:questionId })
+    .then((result)=>{
+      if(result.records.length === 0 ){
+        if (req.headers.accept.includes('application/json')) {
+          res.status(200).json({
+            Question: question,
+          }); 
+        }else {
+          res.render('index', {
+            Question: question,
+          });
+        }
+      } else {
+        var subQuestionsArr =[];
+        result.records.forEach((record) => {
+          var subQuestion = {
+            id: record._fields[0].identity.low,
+            type: record._fields[0].properties.type,
+            questionNumber:record._fields[0].properties.questionNo.low,
+            title: record._fields[0].properties.questionTitle,
+            optionA: record._fields[0].properties.optionA,
+            optionB: record._fields[0].properties.optionB,
+            optionC: record._fields[0].properties.optionC,
+            optionD: record._fields[0].properties.optionD,
+            optionE: record._fields[0].properties.optionE,
+            optionACsc: record._fields[0].properties.optionACsc,
+            optionBCsc: record._fields[0].properties.optionBCsc,
+            optionCCsc: record._fields[0].properties.optionCCsc,
+            optionDCsc: record._fields[0].properties.optionDCsc,
+            optionECsc: record._fields[0].properties.optionECsc
+          }
+          subQuestionsArr.push(subQuestion);
+        })
+          // console.log(subQuestion);
+          if (req.headers.accept.includes('application/json')) {
+            res.status(200).json({
+              Question: question,
+              subQuestions:subQuestionsArr
+            }); 
+          }else {
+            res.render('index', {
+              Question: question,
+              subQuestions:subQuestionsArr
+            });
+          }
+      }    
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      session2.close(); // Close the session
+    });
   })
   .catch((err) => {
     console.log(err);
@@ -138,32 +242,33 @@ app.get('/:category', (req,res) => {
 
 })
 
-// app.post('/submit', (req, res) => {
-//   var title = req.body.text;
-//   // var year = req.body.year;
-//   // console.log(title);
-//   session
-//     .run('MATCH(n:question)-[r {title: $titleParam}]->(m:Question) RETURN m',{titleParam:title})
-//     .then(function(result){
-//       const nextQuestion = result.records[0]._fields[0].properties.Question_id;
-//       if (req.headers.accept.includes('application/json')) {
-//         res.status(200).json({
-//           Question:nextQuestion,
-//         });
-//       } else {
-//         res.redirect('/')
-//       }
-//       // session.close();
-//     })
-//     .catch(function(err){
-//       console.log(err)
-//     })
-
-// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+// res.status(200).json({
+//   response: "work in progress,will get back to you shortly",
+// });
+
+
+        // var subQuestionArr = {
+        //   id: result.records[0]._fields[0].identity.low,
+        //   questionNumber:result.records[0]._fields[0].properties.questionNo,
+
+        //   title: result.records[0]._fields[0].properties.questionTitle,
+        //   optionA: result.records[0]._fields[0].properties.optionA,
+        //   optionB: result.records[0]._fields[0].properties.optionB,
+        //   optionC: result.records[0]._fields[0].properties.optionC,
+        //   optionD: result.records[0]._fields[0].properties.optionD,
+        //   optionE: result.records[0]._fields[0].properties.optionE,
+        //   optionACsc: result.records[0]._fields[0].properties.optionACsc,
+        //   optionBCsc: result.records[0]._fields[0].properties.optionBCsc,
+        //   optionCCsc: result.records[0]._fields[0].properties.optionCCsc,
+        //   optionDCsc: result.records[0]._fields[0].properties.optionDCsc,
+        //   optionECsc: result.records[0]._fields[0].properties.optionECsc
+        // }
 
 
   // let {questionText} = req.query;
@@ -195,3 +300,27 @@ app.listen(port, () => {
 // .catch(function(err){
 //   console.log(err)
 // })
+
+
+// app.post('/submit', (req, res) => {
+//   var title = req.body.text;
+//   // var year = req.body.year;
+//   // console.log(title);
+//   session
+//     .run('MATCH(n:question)-[r {title: $titleParam}]->(m:Question) RETURN m',{titleParam:title})
+//     .then(function(result){
+//       const nextQuestion = result.records[0]._fields[0].properties.Question_id;
+//       if (req.headers.accept.includes('application/json')) {
+//         res.status(200).json({
+//           Question:nextQuestion,
+//         });
+//       } else {
+//         res.redirect('/')
+//       }
+//       // session.close();
+//     })
+//     .catch(function(err){
+//       console.log(err)
+//     })
+
+// });
