100ffdb8eae94e511f5f35b3eafa94bd57edd30d
Yash Raj Mishra <yashrm1406@gmail.com>
2023-06-01 22:42:56 +0530

    added another route

--- server.js
+++ server.js
@@ -243,6 +243,110 @@ app.get('/:category', (req,res) => {
 })
 
 
+app.get('/:category/:qNo', (req,res) => {
+  const category = req.params.category;
+  const questionNumber = req.params.qNo;
+  console.log(questionNumber)
+  var questions = 'questions_AC';
+  var subQuestions = 'subquestions_AC';
+
+  if (category === "ns") {questions = 'questions_NS'; subQuestions = 'subquestions_NS';};
+  if (category === "gc") {questions = 'questions_GC'; subQuestions = "NULL";};
+  if (category === "lg") {questions = 'questions_LG'; subQuestions = "NULL";};
+
+  const questionId = req.params.qNo;
+  const query = `MATCH (n:${questions} {questionId: $questionId}) RETURN n`;
+
+  const session = driver.session({ database: 'neo4j' }); // Create a new session
+
+  session.run(query, { questions, questionId:parseInt(questionId) })
+  .then((result) => {
+    if (result.records.length === 0) {
+      // Handle case when no records are found
+      res.status(404).json({
+        error: "No records found",
+      });
+      return;
+    }
+    var question = {
+      id: result.records[0]._fields[0].identity.low,
+      questionNumber:result.records[0]._fields[0].properties.questionId.low,
+      title: result.records[0]._fields[0].properties.questionTitle,
+      optionA: result.records[0]._fields[0].properties.optionA,
+      optionB: result.records[0]._fields[0].properties.optionB,
+      optionC: result.records[0]._fields[0].properties.optionC,
+      optionD: result.records[0]._fields[0].properties.optionD,
+      optionE: result.records[0]._fields[0].properties.optionE,
+      optionACsc: result.records[0]._fields[0].properties.optionACsc,
+      optionBCsc: result.records[0]._fields[0].properties.optionBCsc,
+      optionCCsc: result.records[0]._fields[0].properties.optionCCsc,
+      optionDCsc: result.records[0]._fields[0].properties.optionDCsc,
+      optionECsc: result.records[0]._fields[0].properties.optionECsc,
+    };
+    session.close()
+    const session2 = driver.session({ database: 'neo4j' }); // Create a new session
+    const subQuery = `MATCH (n:${subQuestions}) where n.questionNo=$questionId return n`
+    session2.run(subQuery, { subQuestions: subQuestions,questionId:parseInt(questionId) })
+    .then((result)=>{
+      if(result.records.length === 0 ){
+        if (req.headers.accept.includes('application/json')) {
+          res.status(200).json({
+            Question: question,
+          }); 
+        }else {
+          res.render('index', {
+            Question: question,
+          });
+        }
+      } else {
+        var subQuestionsArr =[];
+        result.records.forEach((record) => {
+          var subQuestion = {
+            id: record._fields[0].identity.low,
+            type: record._fields[0].properties.type,
+            questionNumber:record._fields[0].properties.questionNo.low,
+            title: record._fields[0].properties.questionTitle,
+            optionA: record._fields[0].properties.optionA,
+            optionB: record._fields[0].properties.optionB,
+            optionC: record._fields[0].properties.optionC,
+            optionD: record._fields[0].properties.optionD,
+            optionE: record._fields[0].properties.optionE,
+            optionACsc: record._fields[0].properties.optionACsc,
+            optionBCsc: record._fields[0].properties.optionBCsc,
+            optionCCsc: record._fields[0].properties.optionCCsc,
+            optionDCsc: record._fields[0].properties.optionDCsc,
+            optionECsc: record._fields[0].properties.optionECsc
+          }
+          subQuestionsArr.push(subQuestion);
+        })
+          // console.log(subQuestion);
+          if (req.headers.accept.includes('application/json')) {
+            res.status(200).json({
+              Question: question,
+              subQuestions:subQuestionsArr
+            }); 
+          }else {
+            res.render('index', {
+              Question: question,
+              subQuestions:subQuestionsArr
+            });
+          }
+      }    
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      session2.close(); // Close the session
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
